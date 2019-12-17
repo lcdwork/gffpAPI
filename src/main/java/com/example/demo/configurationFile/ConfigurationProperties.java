72cdbe319d3a7930171f9f799e01a46584cc69ee
lcd <83559178@qq.com>
2019-12-17 11:41:52 +0800

    static variables use propertiesFile

---
+++ src/main/java/com/example/demo/configurationFile/ConfigurationProperties.java
@@ -0,0 +1,33 @@
+package com.example.demo.configurationFile;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConfigurationProperties {
+
+    public static String regionId;
+    public static String regionKey;
+    public static String secretKey;
+    public static String tokenUrl;
+
+    @Value("${gffp.regionId}")
+    public void setRegionId(String regionId) {
+        this.regionId = regionId;
+    }
+
+    @Value("${gffp.regionKey}")
+    public void setRegionKey(String regionKey) {
+        this.regionKey = regionKey;
+    }
+
+    @Value("${gffp.secretKey}")
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    @Value("${gffp.tokenUrl}")
+    public void setTokenUrl(String tokenUrl) {
+        this.tokenUrl = tokenUrl;
+    }
+}
--- src/main/java/com/example/demo/tools/HttpTools.java
+++ src/main/java/com/example/demo/tools/HttpTools.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.collect.monitor.utils.DESUtils;
 import com.example.demo.webapp.domain.Base;
+import com.example.demo.configurationFile.ConfigurationProperties;
 import com.example.demo.webapp.domain.Data;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.http.HttpEntity;
@@ -13,13 +14,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.web.client.RestTemplate;
 
-import java.text.SimpleDateFormat;
 import java.util.HashMap;
 
 public class HttpTools {
-    public static String regionId = "900090000000";
-    public static String regionKey = "90009";
-    public static String secretKey = "FPDDSZJR";
+
     // 推送数据方法
     @Retryable
     public static String postData(String url, Data data) {
@@ -27,18 +25,13 @@ public class HttpTools {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
-        // REGION_ID、REGION_KEY均由接口提供方提供
-//        String regionId = "900090000000";
-//        String regionKey = "90009";
-//        String secretKey = "FPDDSZJR";
-
         Base base = new Base();
-        base.setRegionId(regionId);
+        base.setRegionId(ConfigurationProperties.regionId);
         base.setTimeStamp(System.currentTimeMillis()/1000+"");
 
 
-        String encryptDataStr = DESUtils.encrypt(JSONObject.toJSONString(data), secretKey);
-        String decryptDateStr = DESUtils.decrypt(encryptDataStr, secretKey);
+        String encryptDataStr = DESUtils.encrypt(JSONObject.toJSONString(data), ConfigurationProperties.secretKey);
+        String decryptDateStr = DESUtils.decrypt(encryptDataStr, ConfigurationProperties.secretKey);
         System.out.println(decryptDateStr);
 
         base.setData(encryptDataStr);
@@ -46,7 +39,7 @@ public class HttpTools {
         sb.append(base.getRegionId());
         sb.append(base.getTimeStamp());
         sb.append(base.getData());
-        sb.append(regionKey);
+        sb.append(ConfigurationProperties.regionKey);
         String cSign = DigestUtils.sha256Hex(sb.toString());
         base.setSign(cSign);
         String json = JSONObject.toJSONString(base);
@@ -63,43 +56,40 @@ public class HttpTools {
     // 获取token方法
     @Retryable
     public static String getToken() {
-        System.out.println(1111111);
-        String url = "http://211.160.73.240:19018/gffp/pv/data/gettoken";
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         Base base = new Base();
-        base.setRegionId(regionId);
+        base.setRegionId(ConfigurationProperties.regionId);
         base.setTimeStamp(System.currentTimeMillis()/1000+"");
 
         HashMap<String, String> map= new HashMap<String, String>();
-        map.put("REGION_KEY",regionKey);
+        map.put("REGION_KEY", ConfigurationProperties.regionKey);
 
         base.setData(JSON.toJSONString(map));
         StringBuilder sb = new StringBuilder();
         sb.append(base.getRegionId());
         sb.append(base.getTimeStamp());
         sb.append(base.getData());
-        sb.append(regionKey);
+        sb.append(ConfigurationProperties.regionKey);
         String cSign = DigestUtils.sha256Hex(sb.toString());
         base.setSign(cSign);
         String json = JSONObject.toJSONString(base);
         try {
             HttpEntity<String> request = new HttpEntity<>(json, headers);
-            ResponseEntity<String> response = restTemplate.postForEntity( url, request , String.class );
+            ResponseEntity<String> response = restTemplate.postForEntity(ConfigurationProperties.tokenUrl, request , String.class );
             JSONObject jsonObject = JSON.parseObject(response.getBody());
             if(jsonObject.getString("resCode").equals("0000")) {
                 return jsonObject.getJSONObject("resData").getString("token");
             }else {
-                throw new Exception(jsonObject.getString("resMsg"));
+                throw new Exception(response.getBody());
             }
             // return response.getBody();
         } catch (Exception e) {
             e.printStackTrace();
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            String res = "{\"resCode\":\"抛出异常\",\"resMsg\":" + e.toString() + ",\"resTime\":"+sdf.format(System.currentTimeMillis())+"}";
-            System.out.println(res);
+            // SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            // String res = "{\"resCode\":\"抛出异常\",\"resMsg\":" + e.toString() + ",\"resTime\":"+sdf.format(System.currentTimeMillis())+"}";
             return null;
         }
     }
